ba055685979e30c73736bb63ee3b5579cbbae8f6
rajataswani <120744253+rajataswani@users.noreply.github.com>
2024-03-03 21:25:57 +0530

    Add files via upload

---
+++ Assignment_2/SourceCode/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Developer", "Youtuber", "Blogger","Coding Tutor"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
